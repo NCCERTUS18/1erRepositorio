c30783d9cb9fa4ceb48d4f58ed78386801756d5b
NCCERTUS18 <nicolasjuliancriadob@gmail.com>
2024-09-18 10:00:04 -0500

    Add files via upload

---
+++ CLASE11/script.js
@@ -0,0 +1,47 @@
+// 9.1 comentarios: Este es un comntario de una sola linea 
+
+/*
+este es un comentario
+de multiples lineas
+*/
+
+// 9.2 escritura en javascript
+console.log("ejemplos de javascript estan en la consola");
+
+
+// 9.3 sentencias
+let x = 10; // ta es una sentencia que asigna el valor 10 a x
+let y = 5; // otra sentencia
+console.log(x + y);
+
+// 9.5 operaciones aritmeticas y expresiones 
+let suma = x + y; // suma
+let resta = x - y; // resta
+let multiplicacion = x * y; // multiplicacion 
+let division = x / y; // division
+console.log("suma:", suma);
+console.log("resta:", resta);
+console.log("multiplicacion:", multiplicacion);
+console.log("division:", division);
+
+// 9.7 javaScript es case sensitive
+let nombre = "Juan"; // 'nombre' es diferente a 'Nombre'
+let Nombre = "Pedro"; // Identificadores distintos
+console.log("Nombre en minuscula:", nombre);
+console.log("Nombre en mayuscula:", Nombre);
+
+// 9.8 tipos de datos 
+let esVerdadero = true; // Booleano
+let numero = 100; // Numero
+let texto = "Hola, mundo"; // string
+console.log("Booleano:", esVerdadero);
+console.log("Numero:", numero);
+console.log("String:", texto);
+
+// 9.10 Numeros
+let numeroEntero = 10; // numero entero
+let numeroDecimal = 10.3; // numero decimal
+console.log("Numero entero:", numeroEntero);
+console.log("Numero:", numeroDecimal);
+console.log(numeroDecimal*numeroEntero);
+
